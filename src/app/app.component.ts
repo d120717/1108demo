011f8fd8eb4f1555e769e106737683a7d535a99f
d120717 <sam120717@gmail.com>
2018-11-10 13:20:30 +0800

    ng-model

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,20 @@
 import { Component } from '@angular/core';
 import { toUnicode } from 'punycode';
 
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'demo';
   appTitle = '練習Todo';
   placeholder = '練習placeholder';
-  background = 'blue'
+  background = 'blue';
 
-    newtodo(inputElement) {
-        // 查看event 屬性
-        // console.log(event);
-        console.log(inputElement.value);
+  newtodo(inputElement) {
+    // 查看event 屬性
+    // console.log(event);
+    console.log(inputElement.value);
   }
 }
-
