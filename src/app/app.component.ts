5b18c03c6a43358ccc1e551133b94fe046f801a2
d120717 <sam120717@gmail.com>
2018-11-10 11:36:58 +0800

    屬性增加[ ] = "變數名稱" 再到ts增加變數數值

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
-import { of, Observable, forkJoin } from 'rxjs';
-import { tap, map, mergeMap, concatMap, delay } from 'rxjs/operators';
+
 
 @Component({
   selector: 'app-root',
@@ -10,44 +9,6 @@ import { tap, map, mergeMap, concatMap, delay } from 'rxjs/operators';
 export class AppComponent {
   title = 'demo';
   appTitle = '練習Todo';
+  placeholder = '練習placeholder';
 }
 
-const obs1 = of([1, 2, 3]).pipe(delay(1700));
-const obs2 = of([4, 5, 6]).pipe(delay(600));
-const obs3 = of([7, 8, 9]).pipe(delay(500));
-
-const displayTime = (message: string) => {
-  const now = new Date();
-    console.log(
-      `${message}: ${now.toLocaleTimeString()}.${now.getMilliseconds()}`);
-};
-
-const showTime = (message: string) => (obs: Observable<any>) => obs.pipe(
-  tap(x => displayTime(message))
-);
-
-displayTime('init sub$');
-
-const sub$ = of([0]).pipe(
-
-  showTime('starting mergeMap'),
-  // mergeMap(e => obs2),
-  mergeMap(e => forkJoin(obs1, obs2, obs3), (e, res) => {
-    console.log(e);
-    console.log(res);
-    displayTime('end mergeMap');
-    return [...e, ...[].concat.apply([], res)];
-  }),
-
-  // showTime('end mergeMap'),
-
-  // showTime('starting concatMap'),
-  // concatMap(e => obs1, (e, res) => {
-  //   console.log([...e, ...res]);
-  //   return [...e, ...res];
-  // }),
-  // showTime('end concatMap'),
-);
-
-
-sub$.subscribe();
